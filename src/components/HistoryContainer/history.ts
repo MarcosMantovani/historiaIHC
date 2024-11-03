c6884712d47031fd4854c1a5771b3c89a2691261
Marcos Vinicius Mantovani <marcospiderplays@gmail.com>
2024-11-03 14:11:11 -0300

    melhoria de textos

--- src/components/HistoryContainer/history.ts
+++ src/components/HistoryContainer/history.ts
@@ -90,7 +90,7 @@ export const history: History[] = [
   {
     name: 'caminho_conversar_jurados',
     image: caminho_conversar_jurados,
-    text: 'Você se aproxima dos jurados, sentindo um misto de nervosismo e empolgação.\n\nEles estão discutindo algo, e você decide se juntar à conversa.\n\nVocê prefere (1) perguntar sobre o estilo musical deles ou (2) compartilhar uma história sobre sua jornada musical?',
+    text: 'Você se aproxima dos jurados, sentindo um misto de nervosismo e empolgação que faz seu coração acelerar. À medida que se aproxima, percebe que eles estão imersos em uma discussão animada sobre os detalhes do projeto que apresentaram. Você hesita por um momento, mas a vontade de participar é maior. Com um sorriso cordial, você decide se juntar à conversa, tentando captar o tom da discussão enquanto se prepara para contribuir com suas ideias e impressões. É uma oportunidade única de mostrar seu conhecimento e paixão pelo que fez, e você não quer deixá-la passar.',
     opcoes: [
       {
         name: 'perguntar_sobre_o_estilo_musical_deles',
@@ -105,7 +105,7 @@ export const history: History[] = [
   {
     name: 'perguntar_sobre_o_estilo_musical_deles',
     image: perguntar_sobre_o_estilo_musical_deles,
-    text: 'Você pergunta sobre o estilo musical que eles preferem. Os jurados ficam felizes em compartilhar suas opiniões e se animam ao falar sobre suas bandas favoritas. A conversa flui, e você sente que ganhou a simpatia deles, ajudando a diminuir sua ansiedade. Quando chega a sua vez de se apresentar, você está mais calmo e confiante.',
+    text: 'Você pergunta sobre o estilo musical que eles preferem, e a resposta dos jurados é instantânea. Eles se iluminam ao compartilhar suas opiniões, seus rostos refletindo a paixão pela música que os une. Cada um começa a falar animadamente sobre suas bandas favoritas, desde os clássicos que moldaram suas juventudes até as novas revelações que têm feito sucesso nas paradas. A conversa flui naturalmente, com risadas e trocas de experiências, e você percebe que esse clima descontraído ajuda a diminuir sua ansiedade. O ambiente se torna mais acolhedor, e você sente que ganhou a simpatia deles. Quando finalmente chega a sua vez de se apresentar, você se sente mais calmo e confiante, pronto para brilhar no palco.',
     opcoes: [
       {
         name: 'caminho_manter_calma',
@@ -127,7 +127,7 @@ export const history: History[] = [
   {
     name: 'caminho_manter_calma',
     image: caminho_manter_calma,
-    text: 'Você respira fundo e se concentra, lembrando-se de que se preparou bem para esse momento.\n\nAo iniciar sua apresentação, sua mão se move com confiança, e a música flui de forma suave e clara.\n\nOs jurados observam atentamente, e você percebe que está conseguindo capturar a atenção deles.\n\nVocê prefere (1) adicionar um solo improvisado ou (2) continuar com a música planejada?',
+    text: 'Você respira fundo e se concentra, lembrando-se de que se preparou bem para esse momento tão aguardado. Todos os treinos, a dedicação e as horas de prática valeram a pena, e agora é hora de brilhar. Ao iniciar sua apresentação, sua mão se move com confiança, e a música flui de forma suave e clara, preenchendo o ambiente com suas notas envolventes.\n\nVocê sente a energia da sala, as expectativas dos jurados pairando no ar. Eles observam atentamente cada movimento, cada expressão sua, e você percebe que está conseguindo capturar a atenção deles de maneira eficaz. À medida que avança, a confiança cresce dentro de você, e cada acorde que toca parece ressoar não apenas em seus ouvidos, mas também em seus corações. É um momento mágico, onde você se conecta profundamente com o público, e sabe que está criando uma experiência inesquecível.',
     opcoes: [
       { name: 'fim_solo_improvisado', text: 'Adicionar um solo improvisado.' },
       {
@@ -139,17 +139,35 @@ export const history: History[] = [
   {
     name: 'fim_solo_improvisado',
     image: fim_solo_improvisado,
-    text: 'Você decide arriscar e adicionar um solo improvisado.\n\nOs jurados ficam impressionados com sua criatividade e habilidade.\n\nAo final da sua apresentação, eles aplaudem de pé e você é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns!'
+    text: 'Você decide arriscar e adicionar um solo improvisado que realmente destaque seu talento e originalidade. Com cada nota, você sente a energia da plateia crescendo, e os jurados observam com expressões de surpresa e admiração. Sua criatividade brilha à medida que você incorpora elementos inesperados em sua performance, mostrando não apenas suas habilidades técnicas, mas também sua capacidade de se conectar emocionalmente com a música.\n\nAo final da sua apresentação, a plateia explode em aplausos, e os jurados se levantam, aplaudindo de pé, visivelmente impressionados com o que acabaram de testemunhar. Com um sorriso de satisfação, você recebe o reconhecimento por sua dedicação e talento, e é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns! Você conquistou não apenas o coração dos jurados, mas também a chance de brilhar ao lado de músicos incríveis!',
+    opcoes: [
+      {
+        name: 'inicio',
+        text: 'Parabéns! Reinicie a história.'
+      }
+    ]
   },
   {
     name: 'fim_musica_planejada',
     image: fim_musica_planejada,
-    text: 'Você decide seguir o plano e continuar com a música que ensaiou.\n\nAo final, os jurados elogiam sua técnica e a maneira como você se manteve focado.\n\nVocê é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns!'
+    text: 'Você decide seguir o plano e continuar com a música que ensaiou. Sua confiança brilha à medida que você toca cada acorde, e logo a plateia começa a se envolver com a sua performance. Ao final, os jurados não apenas elogiam sua técnica impecável, mas também reconhecem a maneira como você se manteve focado e emocionalmente conectado à canção. Eles ressaltam sua habilidade de capturar a essência da música e transmitir isso ao público. Após deliberarem, os jurados anunciam com entusiasmo que você foi escolhido como o novo guitarrista da Tempestade de Aço! Essa é uma conquista que você sempre sonhou. Parabéns! Você acaba de dar um grande passo na sua carreira musical!',
+    opcoes: [
+      {
+        name: 'inicio',
+        text: 'Parabéns! Reinicie a história.'
+      }
+    ]
   },
   {
     name: 'caminho_nervoso_audicao',
     image: caminho_nervoso_audicao,
-    text: 'Você fica tão nervoso que suas mãos começam a tremer, e a primeira nota que toca sai desafinada.\n\nOs jurados olham para você com uma mistura de compaixão e desapontamento.\n\nNo entanto, você tenta se recompor e continuar, mas a pressão é grande e você acaba se perdendo na música.\n\nAo final, você termina sua apresentação, mas sabe que não deu o seu melhor.\n\nDias depois, você recebe a notícia de que não foi escolhido.\n\nMas essa experiência te ensina sobre a importância de controlar o nervosismo e se preparar melhor para a próxima vez.'
+    text: 'Você fica tão nervoso que suas mãos começam a tremer, e a primeira nota que toca sai desafinada.\n\nOs jurados olham para você com uma mistura de compaixão e desapontamento.\n\nNo entanto, você tenta se recompor e continuar, mas a pressão é grande e você acaba se perdendo na música.\n\nAo final, você termina sua apresentação, mas sabe que não deu o seu melhor.\n\nDias depois, você recebe a notícia de que não foi escolhido.\n\nMas essa experiência te ensina sobre a importância de controlar o nervosismo e se preparar melhor para a próxima vez.',
+    opcoes: [
+      {
+        name: 'inicio',
+        text: 'Infelizmente não foi dessa vez. Reinicie a história.'
+      }
+    ]
   },
   {
     name: 'caminho_buscar_conselhos',
@@ -169,7 +187,7 @@ export const history: History[] = [
   {
     name: 'caminho_focar_audicao',
     image: caminho_focar_audicao,
-    text: 'Você decide focar na sua audição, ouvindo atentamente os outros guitarristas.\n\nVocê observa as performances deles e analisa o que funciona e o que não funciona. Ao chegar sua vez, você se sente mais preparado e confiante.',
+    text: 'Você decide focar na sua audição, ouvindo atentamente os outros guitarristas que estão se apresentando. Enquanto eles tocam, você se concentra nas nuances de cada performance, prestando atenção não apenas nas notas que eles escolhem, mas também nas emoções que transmitem através de sua música. Você analisa o que funciona e o que não funciona, observando como cada guitarrista interage com a plateia e como a energia do ambiente muda conforme suas execuções.\n\nAo chegar sua vez, você se sente mais preparado e confiante, embasado nas lições que aprendeu ao observar seus colegas. Sua mente está clara, e você se lembra de cada detalhe que pode agregar à sua apresentação. A experiência de ouvir e aprender com os outros enriquece sua performance, e você está pronto para dar o seu melhor.',
     opcoes: [
       {
         name: 'fim_estilo_diferente',
@@ -184,17 +202,35 @@ export const history: History[] = [
   {
     name: 'fim_estilo_diferente',
     image: fim_estilo_diferente,
-    text: 'Você opta por um estilo totalmente diferente.\n\nOs jurados ficam surpresos e admirados pela sua originalidade.\n\nAo final da sua apresentação, eles aplaudem, e você é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns!'
+    text: 'Você opta por um estilo totalmente diferente, trazendo uma abordagem única e inovadora para a sua performance. Sua escolha de riffs e melodias inusitadas faz com que sua apresentação se destaque, capturando a atenção de todos presentes. Os jurados, surpreendidos pela sua originalidade e pela ousadia de sua execução, trocam olhares admirados enquanto você toca. Cada nota parece ressoar com uma energia contagiante que enche a sala. Ao final da sua apresentação, não há dúvida de que você conquistou o público. Eles se levantam em uníssono, aplaudindo com entusiasmo e reconhecimento. Com isso, você é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns! Essa é uma conquista que com certeza marcará o início de uma nova e emocionante fase em sua carreira musical.',
+    opcoes: [
+      {
+        name: 'inicio',
+        text: 'Parabéns! Reinicie a história.'
+      }
+    ]
   },
   {
     name: 'fim_incorporar_ideias',
     image: fim_incorporar_ideias,
-    text: 'Você decide incorporar algumas ideias que gostou.\n\nOs jurados notam a sua habilidade de adaptação e a conexão com o estilo deles.\n\nAo final, eles elogiam sua apresentação e você é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns!'
+    text: 'Você decide incorporar algumas ideias que gostou.\n\nOs jurados notam a sua habilidade de adaptação e a conexão com o estilo deles.\n\nAo final, eles elogiam sua apresentação e você é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns!',
+    opcoes: [
+      {
+        name: 'inicio',
+        text: 'Parabéns! Reinicie a história.'
+      }
+    ]
   },
   {
     name: 'caminho_focar_emocao',
     image: caminho_focar_emocao,
-    text: 'Você decide focar na emoção e fazer com que cada nota ressoe no coração dos jurados.\n\nDurante sua apresentação, você se conecta com a música e transmite essa energia.\n\nOs jurados parecem hipnotizados e, ao final, eles aplaudem com entusiasmo.\n\nVocê é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns!'
+    text: 'Você decide mergulhar de cabeça na emoção da música, fazendo com que cada nota ressoe profundamente no coração dos jurados. Durante sua apresentação, não apenas toca a guitarra, mas se conecta de forma intensa com a melodia, permitindo que a energia flua através de cada acorde. Os jurados, envolvidos pela sua interpretação apaixonada, parecem hipnotizados pela entrega genuína que você oferece. À medida que você avança, a sala vibra com a sua performance, e a tensão se transforma em um clima de expectativa e admiração. Ao final, os aplausos ecoam como um estrondoso reconhecimento da sua arte, e você é escolhido como o novo guitarrista da Tempestade de Aço! Parabéns! Essa é uma conquista que você sempre sonhou, e agora se torna realidade!',
+    opcoes: [
+      {
+        name: 'inicio',
+        text: 'Parabéns! Reinicie a história.'
+      }
+    ]
   },
   {
     name: 'caminho_focar_tecnica',
@@ -221,7 +257,7 @@ export const history: History[] = [
   {
     name: 'caminho_conversar_competidores',
     image: caminho_conversar_competidores,
-    text: 'Você decide conversar com os outros competidores.\n\nAlguns deles compartilham suas experiências e truques, enquanto outros ficam mais reservados.\n\nVocê aprende algumas dicas úteis, mas também sente a pressão aumentar.\n\nAo final da audição, você percebe que, embora tenha se preparado, a competição é acirrada.\n\nOs jurados reconhecem seu talento, mas você não é escolhido desta vez.\n\nDeseja tentar novamente?',
+    text: 'Você decide se aproximar e conversar com os outros competidores que também estão esperando por sua vez. Enquanto alguns deles estão abertos e dispostos a compartilhar suas experiências e truques, outros optam por ficar mais reservados e concentrados. Você aproveita essa oportunidade para coletar informações valiosas, aprendendo sobre diferentes abordagens e estratégias que podem ser úteis. À medida que as conversas avançam, você percebe que a pressão está aumentando, não apenas pela concorrência, mas também pela atmosfera de expectativa que paira no ar. Ao final da audição, mesmo sabendo que se preparou intensamente para esse momento, a competição se revela mais acirrada do que você imaginava. Os jurados, ao reconhecerem seu talento, fazem alguns comentários encorajadores, mas infelizmente, você não é escolhido desta vez. Sentindo uma mistura de emoções, você se pergunta se deve se arriscar novamente e tentar outra vez em futuras oportunidades. O que você decidirá fazer?',
     opcoes: [
       {
         name: 'inicio',
